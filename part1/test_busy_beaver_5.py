918f985548b3bb9e5392c8eed438365e2cee785d
parnia <parniafazeli2005@gmail.com>
2026-07-05 16:36:52 +0330

    Langton

--- part1/test_busy_beaver_5.py
+++ part1/test_busy_beaver_5.py
@@ -10,24 +10,16 @@ from test_turing_machine_example1 import print_states
 
 transitions = {
     
-('a','0'): ('b','1','R'),
+    ('a','0'): ('b','1','R'),
     ('a','1'): ('b','1','L'),
-
-
     ('b','0'): ('a','1','L'),
     ('b','1'): ('c','0','L'),
-
-
-    ('c','0'): ('e','1','R'),
+    ('c','0'): ('e','0','L'),
     ('c','1'): ('d','1','L'),
-
-
     ('d','0'): ('d','1','R'),
     ('d','1'): ('a','0','R'),
-
-
-    ('e', '0'): ('e', '1', 'R'),
-    ('e', '1'): ('qa', '1', 'L'),
+    ('e','0'): ('e','1','R'),
+    ('e','1'): ('qa','1','L'),
 
 }
 
@@ -38,7 +30,6 @@ if __name__ == "__main__":
 
     print_states(transitions)
 
-
     tm = TuringMachine(
         transitions,
         start_state='a',
@@ -47,6 +38,4 @@ if __name__ == "__main__":
         blank_symbol='0'
 
     )
-
-
     tm.debug("", step_limit=5000)
